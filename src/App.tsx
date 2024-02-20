70e643d9fd6a90b4a66d74258818e058566f9b64
nataliabpo <nataliaoliveir0108@gmail.com>
2024-02-20 15:37:24 -0300

    atualização login

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
           <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
