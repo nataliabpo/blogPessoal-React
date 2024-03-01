5e74d365c0cd23c496130e462b43f2fd34fec878
nataliabpo <nataliaoliveira0108@gmail.com>
2024-03-01 10:36:55 -0300

    Deploy do Projeto Integrador

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -22,7 +22,7 @@ function Navbar() {
           <div className="container flex justify-between text-lg">
             <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
-            <div className='flex gap-4'>
+            <div className='flex gap-4 intems-center'>
               <Link to='/postagens' className='hover:underline'>Postagens</Link>
               <Link to='/temas' className='hover:underline'>Temas</Link>
               <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
